5de80ccf019ca1b648ae71c75919328a1bcfeecb
DDDm3 <91607452+DDDm3@users.noreply.github.com>
2024-11-05 21:11:23 +0700

    Them 12 file

---
+++ Beta 0.1/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_KTMH
+{
+    public abstract class Person
+    {
+        private string name;
+        private string email;
+        private DateTime dateOfBirth;
+        private string phone_num;
+        private string address;
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string Phone_num { get; set; }
+        public string Address { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - DateOfBirth.Year;
+            }
+        }
+
+        protected Person(string name, string email, DateTime dateofbirth, string phone_num, string address)
+        {
+            this.name = name;
+            this.email = email;
+            this.dateOfBirth = dateofbirth;
+            this.phone_num = phone_num;
+            this.address = address;
+        }
+        public void UpdateContactInfo(string phone_num, string address)
+        {
+            Phone_num = phone_num;
+            Address = address;
+        }
+
+        public void UpdatePersonalDetails(string name, string email, DateTime dateOfBirth)
+        {
+            // Add any necessary validation here
+            this.Name = name;
+            this.Email = email;
+            this.DateOfBirth = dateOfBirth;
+        }
+    }
+}
---
+++ Beta 0.1/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_KTMH
+{
+    public class User
+    {
+        private string login_name;
+        private string password;
+
+        public string Login_name { get => login_name; set => login_name = value; }
+        public string Password { get => password; set => password = value; }
+       
+        public User(string loginname, string password)
+        {
+            this.login_name = loginname;
+            this.password = password;
+
+        }
+
+        public bool Login(string username, string password)
+        {
+            if (username == Login_name && password == Password)
+            {
+                Console.WriteLine("Đăng nhập thành công!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Tên đăng nhập hoặc mật khẩu không đúng.");
+                return false;
+            }
+        }
+        //private void btnLogOut_Click(object sender, EventArgs e)
+        //{
+        //    Logout();
+        //}
+
+        //public void Logout()
+        //{
+            
+        //        foreach (sForm form in Application.OpenForms)
+        //        {
+        //            if (form != this) // Giữ lại LoginForm hoặc MainForm
+        //                form.Close();
+        //        }
+        //        this.Hide(); // Ẩn form hiện tại
+        //        UserForm loginForm = new UserForm();
+        //        loginForm.Show();
+        //    Console.WriteLine("Đăng xuất thành công!");
+        //}
+    }
+}
