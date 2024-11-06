e545514156c35fa00d52f72f412751507eb43c9f
Nguyen Quoc Khanh <nqkhanh2925@gmail.com>
2024-11-06 12:19:13 +0700

    chỉnh sửa User và các lớp liên quan
    hoàn thiện hình thức đăng nhập cơ bản

--- [Project] Atendence Program/User.cs
+++ [Project] Atendence Program/User.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace _Project__Atendence_Program
+namespace Project_KTMH
 {
     public class User
     {
         private string login_name;
         private string password;
-        private string employee_ID;
 
         public string Login_name { get => login_name; set => login_name = value; }
         public string Password { get => password; set => password = value; }
-        public string Employee_ID { get => employee_ID; set => employee_ID = value; }
-        public User(string login_name, string password, string employee_ID)
+       
+        public User(string loginname, string password)
         {
-            this.login_name = login_name;
+            this.login_name = loginname;
             this.password = password;
-            this.employee_ID = employee_ID;
+
         }
 
         public bool Login(string username, string password)
@@ -32,14 +34,6 @@ namespace _Project__Atendence_Program
                 Console.WriteLine("Tên đăng nhập hoặc mật khẩu không đúng.");
                 return false;
             }
-        }
-
-        public void Logout()
-        {
-
-
-
-            Console.WriteLine("Đăng xuất thành công!");
-        }
+        }      
     }
 }
