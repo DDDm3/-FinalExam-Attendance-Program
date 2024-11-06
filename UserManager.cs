0db19ba74871dc30f2ee4aae0e312febff2de353
Nguyen Quoc Khanh <nqkhanh2925@gmail.com>
2024-11-06 12:40:20 +0700

    Add files via upload

---
+++ UserManager.cs
@@ -0,0 +1,45 @@
+﻿using Project_KTMH;
+using System.Collections.Generic;
+using System;
+
+public class UserManager
+{
+    private static UserManager instance;
+    private List<User> users;
+
+    private UserManager()
+    {
+        users = new List<User>();
+    }
+    public static UserManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new UserManager();
+            }
+            return instance;
+        }
+    }
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+    public List<User> GetUsers()
+    {
+        return users;
+    }
+    public bool Login(string username, string password)
+    {
+        foreach (User user in users)
+        {
+            if (user.Login(username, password))
+            {
+                return true;
+            }
+        }
+        Console.WriteLine("Tên đăng nhập hoặc mật khẩu không đúng.");
+        return false;
+    }
+}
